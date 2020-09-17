daaa817ee47301d5e9f29797fb12d4b8f2d5e96e
Mario Martin moreno <mmartin-@student.42madrid.com>
2020-09-17 20:50:24 +0200

    long pause commit, not lot of changes

--- srcs/error_handle.c
+++ srcs/error_handle.c
@@ -6,7 +6,7 @@
 /*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/02 17:37:33 by mmartin-          #+#    #+#             */
-/*   Updated: 2020/09/04 20:43:58 by mmartin-         ###   ########.fr       */
+/*   Updated: 2020/09/17 19:10:28 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	print_error(t_errcode errcod)
 	char	current;
 
 	mask = -1;
+	ft_putstr_fd("Error:\n", 1);
 	while ((mask = mask + 5) < 64)
 		if ((current = errcod << mask >> 59))
 			ft_putchar_fd(current == 27 ? ' ' : (current | 96), 1);
--- srcs/read_file.c
+++ srcs/read_file.c
@@ -5,24 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmartin- <mmartin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/04 19:41:25 by mmartin-          #+#    #+#             */
-/*   Updated: 2020/09/04 20:43:55 by mmartin-         ###   ########.fr       */
+/*   Created: 2020/09/16 19:58:11 by mmartin-          #+#    #+#             */
+/*   Updated: 2020/09/17 20:49:54 by mmartin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <libft.h>
+#include <get_next_line.h>
+#include "../includes/minirt.h"
 #include "../includes/error_handle.h"
 
-float	readv(char *ln)
+t_errcode		read_res(t_conf *conf, char const *line)
 {
-	static int	lp = 0;
-	float		final;
-	int			decimal;
+	while (ft_isspace(*line))
+		line++;
+	if (!ft_isdigit(*line))
+		return (CONF_INV_RES);
+	conf->res->x = *line - '0';
+	while (ft_isdigit(*(line + 1)))
+		conf->res->x = conf->res->x * 10 + (*++line - '0');
+	if (!ft_isspace(*++line))
+		return (CONF_INV_RES);
+	while (ft_isspace(*line))
+		line++;
+	if (!ft_isdigit(*line))
+		return (CONF_INV_RES);
+	conf->res->y = *line - '0';
+	while (ft_isdigit(*(line + 1)))
+		conf->res->y = conf->res->y * 10 + (*++line - '0');
+	return (*line ? CONF_INV_RES : 0);
+}
 
-	if ((!ft_isdigit(*(ln + lp - 1)) || !ft_isdigit(*(ln + lp + 1))) &&
-			(*(ln + lp) == ',' || *(ln + lp) == '.'))
+
+
+t_errcode		read_conf(t_conf *conf, char const *path)
+{
+	int const	fd = open(path, O_RDONLY);
+	char		*line;
+	t_errcode	err;
+
+	if (fd < 0)
+		return (CONF_MISSING);
+	while (get_next_line(fd, &line) == 1)
 	{
-		// TODO error handle
-		return (0.0f);
+		if (*line == 'R')
+			err = read_res(conf, line);
+		if 
+		free(line);
 	}
-	return (final);
+	if (line)
+		free(line);
+	return (err = close(fd) < 0 ? CONF_MISSING : err);
 }
